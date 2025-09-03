7ef7b824f099ecf1f20ad71ff7fdc5e2db464f07
Kanishka Sharma <kanishkasharma@Kanishkas-MacBook-Air-2.local>
2025-09-04 00:45:19 +0530

    tried fixing video again

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='w-full h-full'>
-         <video className='h-full w-full object-cover' autoPlay loop muted src="/Video.mp4" type="video/mp4"></video>
+      <video className="h-full w-full object-cover" autoPlay loop muted playsInline>
+  <source src="/Video.mp4" type="video/mp4" />
+</video>
+
     </div>
   )
 }
